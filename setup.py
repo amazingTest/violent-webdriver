2b7c47dc449c809b02888963542e65a3ff7c32da
shaoyuyishiwo <523314409@qq.com>
2018-06-26 21:38:09 +0800

    modify version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="violent_webdriver",
-    version="1.0.1",
+    version="1.0.2",
     author="Yuyi Shao",
     author_email="523314409@qq.com",
     description="violent webdriver based on selenium",
