b6120e25aeb71cc9c797059f32ce51421556d3f6
amazingTest <523314409@qq.com>
2018-06-27 16:16:43 +0800

    add examples

---
+++ examples/is_page_refreshed_example.py
@@ -0,0 +1,10 @@
+from violent_webdriver import Chrome
+
+# the value of executable_path should be your own path
+dr = Chrome.violent_chromedriver(executable_path='C://MyDownloads/Download/chrome-win32/chromedriver.exe')
+dr.get('https://www.baidu.com')
+dr.implicitly_wait(30)
+trigger = dr.find_element_by_name('tj_trtieba')
+dr.v_send_keys(locate_rule=[['tag', 'input'], ['class', 's_ipt']], message='test')
+dr.v_click(locate_rule=[['tag', 'input'], ['value', '百度一下']])
+print(dr.is_page_refreshed(trigger))
