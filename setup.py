35f86d05a7d78d03394df0cac3d2044d4034ab9d
amazingTest <523314409@qq.com>
2018-07-02 15:11:34 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="violent-webdriver",
-    version="1.0.16",
+    version="1.0.17",
     author="Yuyi Shao",
     author_email="523314409@qq.com",
     description="violent webdriver based on selenium",
--- violent_webdriver/Chrome.py
+++ violent_webdriver/Chrome.py
@@ -39,7 +39,6 @@ class violent_chromedriver(webdriver.Chrome):
             mobile_emulation = {
                 "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
                 "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
-            chrome_options = Options()
             chrome_options.add_argument('disable-infobars')
             chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 
